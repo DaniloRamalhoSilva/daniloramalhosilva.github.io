68d78c8f9817e90d59b81320e02a325dd3bdaf32
Danilo Ramalho <daniloti@hotmail.com>
2022-07-30 13:42:27 -0300

    mudando caminho repositorio carrinho de compras

--- carrinho_de_compras/helpers/saveCartItems.js
+++
@@ -1,8 +0,0 @@
-const saveCartItems = (itens) => {
-  // seu código aqui 
-  window.localStorage.setItem('cartItems', itens);  
-};
- 
-if (typeof module !== 'undefined') {
-  module.exports = saveCartItems;
-}
