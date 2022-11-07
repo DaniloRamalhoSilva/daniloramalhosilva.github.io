539ee948c5cba6082d405a60ffc283f3907dbc75
Danilo Ramalho <daniloti@hotmail.com>
2022-11-07 18:07:51 -0300

    Refatorando home e aplicando css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,15 @@
 import React from 'react';
 import foto from '../Images/69921664.jpeg'
-import { Row, Col, Figure } from 'react-bootstrap';
 
 function Home() {
   return (
-    <Row >   
-      <Figure >
-        <Figure.Image
-        width="60%"
-        fluido
-        roundedCircle
-        alt="171x180"
-        src={foto}
-        />
-      </Figure>      
-      <Col className='testCol'>
-        <h3>Aqui é o lugar certo para conhecer meu trabalho e tudo sobre mim.</h3>
-        <br />        
-        <h6>Seja muito bem vindo!</h6>
+    <div className='containerHome' >
+      <img className='fotoHome' src={foto} alt="" />
+      <h6>Seja muito bem vindo!</h6>
+      <br /> 
+      <h3>Aqui é o lugar certo para conhecer meu trabalho e <s>tudo</s> sobre mim.</h3>
+          
+      <div className='texto'>
         <hr />
         <p>
           Sou Danilo Ramalho Silva, 32 anos, de São Paulo Capital, apaoixonado por tecnologia e desenvolvimento pessoal.
@@ -26,13 +18,13 @@ function Home() {
           Me formei em Analise e Desenvolvimento de Sisteme em 2016 e atualmente estou me especializando em desenvolvimento web na Trybe com o intuito de me tornar um desenvolvedor full steck.
         </p>
         <p>
-          Se quiser saber um poquinho mais de como vim parar na programação e conhecer minha hestoria um pouco melhor rola ai até o “Sobre mim” você acaba de se tornar meu convidado especial.
+          Se quiser saber um poquinho mais de como vim parar na programação e conhecer minha historia um pouco melhor, rola ai até o “Sobre mim”, você acaba de se tornar meu convidado especial.
         </p>
         <p>
-          Por aqui voce tambem pode ver meus trabalhos proficionais e academicos, minhas steks, formação academica, ultimos trabalho e ate algus dos meus hobs, sinta-se a vontade a casa site e todo seu.
+          Por aqui voce tambem pode ver meus trabalhos proficionais e academicos, minhas skills, formação academica, ultimos trabalho e ate algus dos meus hobs, sinta-se a vontade a <s>casa</s> site e todo seu.
         </p>
-      </Col>      
-    </Row>     
+      </div>      
+    </div>     
   );
 }
 
